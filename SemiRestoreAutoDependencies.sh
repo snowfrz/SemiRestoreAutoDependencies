e2b2492a0819bdefdb09e46f97423c564670ffdc
King Sn0wCh1ld <nojailbreakapps@outlook.com>
2016-06-07 00:21:01 -0400

    Update SemiRestoreAutoDependencies.sh

--- SemiRestoreAutoDependencies.sh
+++ SemiRestoreAutoDependencies.sh
@@ -1,6 +1,6 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo chown -RW "$USER":admin /usr/local
-sudo chown -RW "$USER":admin /Library/Caches/Homebrew
+sudo chown -R "$USER":admin /usr/local
+sudo chown -R "$USER":admin /Library/Caches/Homebrew
 brew install libssh2 gtk+3
 brew link --overwrite gdk-pixbuf
 brew link —-overwrite libssh2
